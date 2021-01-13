a3227bac17fa5f2e0420ef292fa60912835e199c
ZD Hu <zd@dinngo.co>
2021-01-13 12:05:12 +0800

    Update v1.0.0 contract addresses

--- pkg/ethereum/furucombo/furucombo_test.go
+++ pkg/ethereum/furucombo/furucombo_test.go
@@ -8,15 +8,15 @@ import (
 )
 
 func TestFurucomboAddresses(t *testing.T) {
-	os.Setenv("FURUCOMBO_PROXY_ADDRESS", "0xEEbeC7874f46C4452610A11FA6bE0264f7f0557F")
+	os.Setenv("FURUCOMBO_PROXY_ADDRESS", "0x17e8Ca1b4798B97602895f63206afCd1Fc90Ca5f")
 	proxyAddress := ProxyAddress()
-	assert.Equal(t, "0xEEbeC7874f46C4452610A11FA6bE0264f7f0557F", proxyAddress.String())
+	assert.Equal(t, "0x17e8Ca1b4798B97602895f63206afCd1Fc90Ca5f", proxyAddress.String())
 	proxyAddressHash := ProxyAddressHash()
-	assert.Equal(t, "0x000000000000000000000000eebec7874f46c4452610a11fa6be0264f7f0557f", proxyAddressHash.String())
+	assert.Equal(t, "0x00000000000000000000000017e8ca1b4798b97602895f63206afcd1fc90ca5f", proxyAddressHash.String())
 
-	os.Setenv("FURUCOMBO_DSPROXY_ADDRESS", "0xa5c1D410bA1a2839591A9672CC4e0e0281b23b1c")
+	os.Setenv("FURUCOMBO_DSPROXY_ADDRESS", "0xa47aC1dd1cF8513B2eB5ca8baD5B8DA93492bF02")
 	dsProxyAddress := DSProxyAddress()
-	assert.Equal(t, "0xa5c1D410bA1a2839591A9672CC4e0e0281b23b1c", dsProxyAddress.String())
+	assert.Equal(t, "0xa47aC1dd1cF8513B2eB5ca8baD5B8DA93492bF02", dsProxyAddress.String())
 	dsProxyAddressHash := DSProxyAddressHash()
-	assert.Equal(t, "0x000000000000000000000000a5c1d410ba1a2839591a9672cc4e0e0281b23b1c", dsProxyAddressHash.String())
+	assert.Equal(t, "0x000000000000000000000000a47ac1dd1cf8513b2eb5ca8bad5b8da93492bf02", dsProxyAddressHash.String())
 }
