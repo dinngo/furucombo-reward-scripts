907cf818a33ddd5e141475fdf4e701dad3e181e7
ZD Hu <zd@dinngo.co>
2021-02-01 16:57:42 +0800

    Add oneinch v2 test

---
+++ pkg/rewarder/cube_finder_oneinchv2_test.go
@@ -0,0 +1,83 @@
+package rewarder
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/dinngodev/furucombo-reward-scripts/pkg/ethereum"
+)
+
+func TestFindOneInchV2SwapCube(t *testing.T) {
+	TestLoadDotEnv(t)
+	TestResetTokenWhitelistFilepath(t)
+
+	testCases := []struct {
+		txHash   common.Hash
+		expected []struct {
+			index        int
+			tokenAddress string
+			tokenAmount  string
+		}
+	}{
+		// ETH
+		{
+			txHash: common.HexToHash("0x8b67eb7e59b8f13c20e6b4961bb8104921b92ccf83405c13886bce9000e7efcd"),
+			expected: []struct {
+				index        int
+				tokenAddress string
+				tokenAmount  string
+			}{
+				{
+					index:        11,
+					tokenAddress: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+					tokenAmount:  "10000000000000000000",
+				},
+				{
+					index:        15,
+					tokenAddress: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+					tokenAmount:  "10000000000000000000",
+				},
+			},
+		},
+		// COMBO
+		{
+			txHash: common.HexToHash("0x573275b8a66104e138a1c6884c420eee3cacdddf70b9418cc7cba97e45ef5c26"),
+			expected: []struct {
+				index        int
+				tokenAddress string
+				tokenAmount  string
+			}{
+				{
+					index:        5,
+					tokenAddress: "0xfFffFffF2ba8F66D4e51811C5190992176930278",
+					tokenAmount:  "5266460501000732",
+				},
+			},
+		},
+	}
+
+	for i, testCase := range testCases {
+		t.Run(fmt.Sprintf("TestCase[%d]", i+1), func(t *testing.T) {
+			receipt, err := ethereum.Client().TransactionReceipt(context.Background(), testCase.txHash)
+			assert.Nil(t, err)
+
+			count := 0
+			for i, txLog := range receipt.Logs {
+				cube, err := findOneInchV2SwapCube(txLog)
+				assert.Nil(t, err)
+
+				if cube != nil {
+					assert.Equal(t, testCase.expected[count].index, i)
+					assert.Equal(t, testCase.expected[count].tokenAddress, cube.TokenAddress.String())
+					assert.Equal(t, testCase.expected[count].tokenAmount, cube.TokenAmount.String())
+					count++
+				}
+			}
+			assert.Equal(t, len(testCase.expected), count)
+		})
+	}
+}
